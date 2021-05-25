08aa5314f5e07a9a11f0c72c0de9896c11d8909e
Noah Wager <nwager@cs.washington.edu>
2021-05-24 22:20:23 -0700

    Flying whale with Sonic hoops

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,33 @@ import './css/App.css';
 import Title from "./Title";
 import Renderer from "./Renderer";
 
-class App extends Component {
+interface AppState {
+  score: number;
+}
+
+class App extends Component<{}, AppState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      score: 0
+    }
+  }
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    })
+  }
 
   render() {
     return (
       <div className="App">
-        <Renderer />
+        <Renderer onScore={this.incrementScore} />
         <Title subtitle="Subtitle from props" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>This is some body text.</p>
+          <p>Score: {this.state.score}</p>
         </header>
       </div>
     );
--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -6,14 +6,23 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './css/Renderer.css'
 import skyURL from './resources/sky_bg.jpg';
 
-interface RendererState {
-  mount: HTMLDivElement;
+interface RendererProps {
+  onScore(): void;
 }
 
-class Renderer extends Component<{}, RendererState> {
+const orbitControls = false;
+const distance = 50;
+const r = 30;
+const omega = 1 / 0.5;
+const epsilon = 1
+
+class Renderer extends Component<RendererProps> {
+
+  // change some of these to state?
 
   private mount: HTMLDivElement | null = null;
   private whaleMesh: THREE.Object3D | null = null;
+  private mixer: THREE.AnimationMixer | null = null;
 
 	componentDidMount() {
 
@@ -27,39 +36,48 @@ class Renderer extends Component<{}, RendererState> {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    camera.position.setZ(30);
-    camera.position.setX(-3);
+    camera.position.x = -distance;
+    camera.position.z = distance;
+    camera.position.y = distance/2;
+    camera.rotation.x -= this.degToRad(26.56);
+    camera.rotation.y = this.degToRad(-45);
+
+    const clock = new THREE.Clock();
 
     renderer.render(scene, camera);
 
-    // Torus
-
-    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-    const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-    const torus = new THREE.Mesh(geometry, material);
-
-    scene.add(torus);
-
     // Lights
 
-    const pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(5, 5, 5);
-
-    const ambientLight = new THREE.AmbientLight(0xffffff);
-    scene.add(pointLight, ambientLight);
+    this.addLights(scene);
 
     // Helpers
 
-    const lightHelper = new THREE.PointLightHelper(pointLight)
-    const gridHelper = new THREE.GridHelper(200, 50);
-    scene.add(lightHelper, gridHelper)
+    const axesHelper = new THREE.AxesHelper(10);
+    scene.add( axesHelper );
+
+    // Torus
+
+    const geometry = new THREE.TorusGeometry(8, 1, 16, 100);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffcb2e });
+    const torus = new THREE.Mesh(geometry, material);
+    torus.position.y = r;
+    torus.rotation.x = this.degToRad(15);
+    scene.add(torus);
 
     // Whale
 
     const loader = new GLTFLoader();
-    loader.load(process.env.PUBLIC_URL + '/models/bluewhale_edited.glb', gltf => {
-      this.whaleMesh = gltf.scene;
+    loader.load(this.pubURL('/models/bluewhale_textured.glb'), gltf => {
+
+      const mesh = gltf.scene;
+      mesh.position.y = r;
+      this.whaleMesh = mesh;
+
+      this.mixer = new THREE.AnimationMixer(this.whaleMesh);
+      this.mixer.clipAction(gltf.animations[7]).play(); // main animation
+
       scene.add(this.whaleMesh);
+
     }, undefined, error => {
       console.log("That's a whale of an error!");
       console.log(error);
@@ -73,19 +91,61 @@ class Renderer extends Component<{}, RendererState> {
     var animate = () => {
       requestAnimationFrame( animate );
 
-      torus.rotation.x += 0.01;
-      torus.rotation.y += 0.005;
-      torus.rotation.z += 0.01;
+      if (this.mixer) { this.mixer.update(clock.getDelta()); }
 
       if (this.whaleMesh) {
-        this.whaleMesh.rotation.z += 0.01;
+        const phi = Math.PI / 2; // start at top of circle
+        const t = clock.getElapsedTime();
+        const y = r * Math.sin(-omega * t + phi);
+        const z = r * Math.cos(-omega * t + phi);
+        this.whaleMesh.position.set(0, y, z);
+        const period = 2 * Math.PI / omega;
+        const frac = (t % period) / period;
+        this.whaleMesh.rotation.x = frac * Math.PI * 2;
+
+        if (this.whaleMesh.position.distanceTo(torus.position) < epsilon) {
+          const yzRotX = this.torusCircleState(Math.random() * 360);
+          torus.position.set(0, yzRotX[0], yzRotX[1]);
+          torus.rotation.set(yzRotX[2], 0, 0);
+
+          this.props.onScore();
+        }
       }
 
       renderer.render( scene, camera );
 
-      controls.update();
+      if (orbitControls) {controls.update();}
     };
     animate();
+  }
+
+  addLights(scene: THREE.Scene) {
+    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    hemiLight.position.set( 0, 300, 0 );
+    hemiLight.intensity = 2;
+    scene.add( hemiLight );
+
+    var dirLight = new THREE.DirectionalLight( 0xffffff );
+    dirLight.position.set( 75, 300, -75 );
+    dirLight.intensity = 2;
+    scene.add( dirLight );
+
+    var ambientLight = new THREE.AmbientLight( 0xffffff );
+    ambientLight.intensity = 3;
+    scene.add(ambientLight);
+  }
+
+  pubURL(path: string): string { return process.env.PUBLIC_URL + path }
+
+  degToRad(deg: number): number { return deg * (Math.PI / 180); }
+  radToDeg(rad: number): number { return rad * (180 / Math.PI); }
+
+  torusCircleState(deg: number): [number, number, number] {
+    const z = r * Math.cos(this.degToRad(deg));
+    const y = r * Math.sin(this.degToRad(deg));
+
+    const rotX = this.degToRad(-deg + 90)
+    return [y, z, rotX];
   }
 
   render() {
